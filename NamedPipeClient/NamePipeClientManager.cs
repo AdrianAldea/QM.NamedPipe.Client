3dcccfd5122c950f6302ca10a2765a45bcb6e78f
AdrianAldea <suntadrian@gmail.com>
2023-05-03 12:58:59 +0300

    First Commit
    v0

---
+++ NamedPipeClient/NamePipeClientManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipeClient
+{
+    public class NamePipeClientManager
+    {
+        private NamedPipeClientStream _namedPipeClientStream = null;
+        const int BUFFERSIZE = 256;
+        private byte[] rcvBuffer;
+
+        public NamePipeClientManager()
+        {
+            rcvBuffer = new byte[BUFFERSIZE];
+
+            _namedPipeClientStream = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut, PipeOptions.Asynchronous);
+            _namedPipeClientStream.Connect();
+
+            Console.WriteLine("Before Factory");
+            Task.Factory.StartNew(() => Recv());
+            Console.WriteLine("After Factory");
+        }
+
+        public void Send(string message)
+        {
+            byte[] _buffer = Encoding.UTF8.GetBytes(message);
+            _namedPipeClientStream.Write(_buffer, 0, _buffer.Length);
+        }
+
+        private void Recv()
+        {
+            while (true)
+            {
+                Console.WriteLine("Before Read");
+                int result = _namedPipeClientStream.Read(rcvBuffer, 0, BUFFERSIZE);
+                string line = Encoding.UTF8.GetString(rcvBuffer, 0, result);
+                Console.WriteLine("Received bytes: " + result + " Response: " + line);
+                Console.WriteLine("After Read");
+            }
+
+        }
+    }
+
+
+}
---
+++ NamedPipeClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamePipeClientManager clientManager = new NamePipeClientManager();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "bye")
+                    break;
+                Console.WriteLine("Before send");
+                clientManager.Send(line);
+                Console.WriteLine("After send");
+            }
+        }
+
+    }
+}
